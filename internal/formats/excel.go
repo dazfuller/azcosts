88deba683a81f296217f98b73675b29a101fbd81
Darren Fuller <darren@elastacloud.com>
2024-02-23 09:24:30 +0000

    Improve Excel formatting in cost summary script
    This commit introduces improvements to the Excel formatting in the cost summary script. It adds functions to create headers, add cost data to sheets, set sheet formats and add tables. In addition, it optimized error handling by shifting from using Error() method to directly passing the error variable.

--- internal/formats/excel.go
+++ internal/formats/excel.go
@@ -7,6 +7,8 @@ import (
 	"log"
 )
 
+const firstCol = "A"
+
 type ExcelFormatter struct {
 	outputPath string
 }
@@ -34,6 +36,38 @@ func (ef ExcelFormatter) Generate(costs []model.ResourceGroupSummary) error {
 	if err != nil {
 		return err
 	}
+
+	err = ef.addHeaders(f, sheetName, costs[0])
+	if err != nil {
+		return err
+	}
+
+	err = ef.addCostDataToSheet(f, sheetName, costs)
+	if err != nil {
+		return err
+	}
+
+	cols, _ := f.GetCols(sheetName)
+	lastCol, _ := excelize.ColumnNumberToName(len(cols))
+
+	err = ef.setSheetFormats(f, sheetName, cols)
+	if err != nil {
+		return err
+	}
+
+	err2 := ef.addTable(f, sheetName, costs, lastCol)
+	if err2 != nil {
+		return err2
+	}
+
+	if err := f.SaveAs(ef.outputPath); err != nil {
+		return fmt.Errorf("an error occured saving the workbook: %s", err.Error())
+	}
+
+	return nil
+}
+
+func (ef ExcelFormatter) addHeaders(f *excelize.File, sheetName string, costEntry model.ResourceGroupSummary) error {
 	firstCell, _ := excelize.JoinCellName("A", 1)
 
 	headers := []string{
@@ -41,17 +75,21 @@ func (ef ExcelFormatter) Generate(costs []model.ResourceGroupSummary) error {
 		"Subscription",
 	}
 
-	for _, bp := range costs[0].Costs {
+	for _, bp := range costEntry.Costs {
 		headers = append(headers, bp.Period)
 	}
 
 	headers = append(headers, "Total Cost")
 
-	err = f.SetSheetRow(sheetName, firstCell, &headers)
+	err := f.SetSheetRow(sheetName, firstCell, &headers)
 	if err != nil {
-		return fmt.Errorf("unable to set header row in costs sheet: %s", err.Error())
+		return fmt.Errorf("unable to set header row in costs sheet: %v", err)
 	}
 
+	return nil
+}
+
+func (ef ExcelFormatter) addCostDataToSheet(f *excelize.File, sheetName string, costs []model.ResourceGroupSummary) error {
 	for i, entry := range costs {
 		rowStart, _ := excelize.JoinCellName("A", i+2)
 		row := []interface{}{
@@ -65,12 +103,15 @@ func (ef ExcelFormatter) Generate(costs []model.ResourceGroupSummary) error {
 
 		row = append(row, entry.TotalCost)
 
-		err = f.SetSheetRow(sheetName, rowStart, &row)
+		err := f.SetSheetRow(sheetName, rowStart, &row)
 		if err != nil {
-			return fmt.Errorf("unable to add data row to costs worksheet: %s", err.Error())
+			return fmt.Errorf("unable to add data row to costs worksheet: %v", err)
 		}
 	}
+	return nil
+}
 
+func (ef ExcelFormatter) setSheetFormats(f *excelize.File, sheetName string, cols [][]string) error {
 	customNumFmt := "#,##0.00;(#,##0.00);-"
 
 	billingStyle, _ := f.NewStyle(&excelize.Style{
@@ -78,10 +119,6 @@ func (ef ExcelFormatter) Generate(costs []model.ResourceGroupSummary) error {
 			Horizontal: "right",
 		},
 	})
-
-	cols, _ := f.GetCols(sheetName)
-	firstCol := "A"
-	lastCol, _ := excelize.ColumnNumberToName(len(cols))
 
 	for i := range cols {
 		colName, _ := excelize.ColumnNumberToName(i + 1)
@@ -92,26 +129,30 @@ func (ef ExcelFormatter) Generate(costs []model.ResourceGroupSummary) error {
 					maxLength = len(v)
 				}
 			}
-			err = f.SetColWidth(sheetName, colName, colName, float64(maxLength)*0.9)
+			err := f.SetColWidth(sheetName, colName, colName, float64(maxLength)*0.9)
 			if err != nil {
-				return fmt.Errorf("unable to set column width for column %s: %s", colName, err.Error())
+				return fmt.Errorf("unable to set column width for column %s: %v", colName, err)
 			}
 			continue
 		}
-		err = f.SetColStyle(sheetName, colName, billingStyle)
+		err := f.SetColStyle(sheetName, colName, billingStyle)
 		if err != nil {
-			return fmt.Errorf("unable to set column style for column %s: %s", colName, err.Error())
+			return fmt.Errorf("unable to set column style for column %s: %v", colName, err)
 		}
 		err = f.SetColWidth(sheetName, colName, colName, 15)
 		if err != nil {
-			return fmt.Errorf("unable to set column width for column %s: %s", colName, err.Error())
+			return fmt.Errorf("unable to set column width for column %s: %v", colName, err)
 		}
 	}
 
+	return nil
+}
+
+func (ef ExcelFormatter) addTable(f *excelize.File, sheetName string, costs []model.ResourceGroupSummary, lastCol string) error {
 	showHeaderRow := true
 	showRowStripes := true
 
-	err = f.AddTable(sheetName, &excelize.Table{
+	err := f.AddTable(sheetName, &excelize.Table{
 		Range:          fmt.Sprintf("%s%d:%s%d", firstCol, 1, lastCol, len(costs)+1),
 		Name:           "CostSummary",
 		StyleName:      "TableStyleMedium9",
@@ -119,11 +160,7 @@ func (ef ExcelFormatter) Generate(costs []model.ResourceGroupSummary) error {
 		ShowRowStripes: &showRowStripes,
 	})
 	if err != nil {
-		return fmt.Errorf("unable to add table to costs sheet: %s", err.Error())
-	}
-
-	if err := f.SaveAs(ef.outputPath); err != nil {
-		return fmt.Errorf("an error occured saving the workbook: %s", err.Error())
+		return fmt.Errorf("unable to add table to costs sheet: %v", err)
 	}
 
 	return nil
