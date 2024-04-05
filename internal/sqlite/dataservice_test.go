bc0f3a0843ee534621d5e9891346446a38abbd25
Darren Fuller <darren@elastacloud.com>
2024-04-05 19:44:23 +0100

    Refactor sqlite test to handle multiple scenarios
    The sqlite testing suite has been refactored to provide a more comprehensive set of test cases. Multiple scenarios have been included, covering different possible outcomes. It also now includes cleanup of databases after tests and better error handling/reporting.

--- internal/sqlite/dataservice_test.go
+++ internal/sqlite/dataservice_test.go
@@ -1,24 +1,83 @@
 package sqlite
 
 import (
-	"github.com/stretchr/testify/assert"
+	"fmt"
+	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
 )
 
 func TestNewCostManagementStore(t *testing.T) {
-	test := assert.New(t)
-	file, err := os.CreateTemp("", "tempdb")
-	if err != nil {
-		test.FailNowf("could not create temp database", "%v", err)
+	tests := []struct {
+		Name                 string
+		DbPath               string
+		Truncate             bool
+		CreateBeforeTruncate bool
+		Expected             error
+		CleanDb              bool
+	}{
+		{
+			Name:                 "Valid path no truncate",
+			DbPath:               "./test.db",
+			Truncate:             false,
+			CreateBeforeTruncate: false,
+			Expected:             nil,
+			CleanDb:              true,
+		},
+		{
+			Name:                 "Valid path with truncate no existing file",
+			DbPath:               "./test.db",
+			Truncate:             true,
+			CreateBeforeTruncate: false,
+			Expected:             nil,
+			CleanDb:              true,
+		},
+		{
+			Name:                 "Valid path with truncate with existing file",
+			DbPath:               "./test.db",
+			Truncate:             true,
+			CreateBeforeTruncate: true,
+			Expected:             nil,
+			CleanDb:              true,
+		},
+		{
+			Name:                 "Invalid path no truncate",
+			DbPath:               "./does/not/exist/test.db",
+			Truncate:             false,
+			CreateBeforeTruncate: false,
+			Expected:             fmt.Errorf("unable to open database file"),
+			CleanDb:              false,
+		},
+		{
+			Name:                 "Invalid path with truncate",
+			DbPath:               "./does/not/exist/test.db",
+			Truncate:             true,
+			CreateBeforeTruncate: false,
+			Expected:             fmt.Errorf("unable to open database file"),
+			CleanDb:              false,
+		},
 	}
-	defer os.Remove(file.Name())
 
-	store, err := NewCostManagementStore(file.Name(), false)
-	if err != nil {
-		test.FailNowf("could not create temp database", "%v", err)
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			if test.CreateBeforeTruncate {
+				_, err := os.Create(test.DbPath)
+				if err != nil {
+					t.Fatalf("Failed to create database file: %v", err)
+				}
+			}
+
+			_, err := NewCostManagementStore(test.DbPath, test.Truncate)
+
+			if test.Expected != nil {
+				require.ErrorContains(t, err, test.Expected.Error())
+			} else {
+				require.NoError(t, err)
+			}
+
+			if test.CleanDb {
+				os.Remove(test.DbPath)
+			}
+		})
 	}
-	defer store.Close()
-
-	test.Equal(store.dbPath, file.Name())
 }
